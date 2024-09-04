8de79125c2c23e45c18dfbc140c3382c2e305a1b
rhea <155525939+dev-Rhea@users.noreply.github.com>
2024-09-04 14:59:08 +0900

    :zap: Refactor : Entity -> DTO 사용하도록 변경

--- src/main/java/bigbrother/slimdealz/repository/Product/ProductRepositoryCustom.java
+++ src/main/java/bigbrother/slimdealz/repository/Product/ProductRepositoryCustom.java
@@ -1,7 +1,7 @@
 package bigbrother.slimdealz.repository.Product;
 
+import bigbrother.slimdealz.dto.product.ProductDto;
 import bigbrother.slimdealz.entity.product.Product;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,20 +9,20 @@ import java.util.List;
 @Repository
 public interface ProductRepositoryCustom {
     // 키워드 검색
-    List<Product> searchByKeyword(String keyword, Long lastSeenId, int size);
+    List<ProductDto> searchByKeyword(String keyword, Long lastSeenId, int size);
 
     // 오늘의 최저가 상품
-    List<Product> findLowestPriceProducts();
+    List<ProductDto> findLowestPriceProducts();
 
     // 상품 상세 페이지
-    Product findProductWithLowestPriceByName(String productName);
+    ProductDto findProductWithLowestPriceByName(String productName);
 
     // 상품 목록
-    List<Product> findByCategory(String category, Long lastSeenId, int size);
+    List<ProductDto> findByCategory(String category, Long lastSeenId, int size);
 
     // 판매처 리스트
-    List<Product> findProductWithVendors(String productName);
+    List<ProductDto> findProductWithVendors(String productName);
 
     // 랜덤 추천
-    List<Product> findRandomProducts();
+    List<ProductDto> findRandomProducts();
 }
--- src/main/java/bigbrother/slimdealz/repository/Product/ProductRepositoryImpl.java
+++ src/main/java/bigbrother/slimdealz/repository/Product/ProductRepositoryImpl.java
@@ -1,5 +1,7 @@
 package bigbrother.slimdealz.repository.Product;
 
+import bigbrother.slimdealz.dto.product.ProductConverter;
+import bigbrother.slimdealz.dto.product.ProductDto;
 import bigbrother.slimdealz.entity.product.Product;
 import bigbrother.slimdealz.entity.product.QPrice;
 import bigbrother.slimdealz.entity.product.QProduct;
@@ -8,7 +10,7 @@ import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
-import org.springframework.cglib.core.Local;
+import org.hibernate.validator.internal.constraintvalidators.bv.time.pastorpresent.PastOrPresentValidatorForCalendar;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -16,6 +18,7 @@ import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.List;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 
 @Repository
@@ -29,6 +32,7 @@ public class ProductRepositoryImpl implements ProductRepositoryCustom {
 
     private final LocalDateTime startOfDay = LocalDate.now().atStartOfDay();
     private final LocalDateTime endOfDay = LocalDate.now().atTime(23, 59, 59, 999999999);
+    private final PastOrPresentValidatorForCalendar pastOrPresentValidatorForCalendar;
 
     // 현재 데이터 없는 경우, 과거 데이터 조회
     private List<Product> backToDayList(LocalDateTime startOfDay, LocalDateTime endOfDay, Function<JPAQueryFactory, List<Product>> queryFunction) {
@@ -49,65 +53,76 @@ public class ProductRepositoryImpl implements ProductRepositoryCustom {
 
     // 검색 결과 목록
     @Override
-    public List<Product> searchByKeyword(String keyword, Long lastSeenId, int size) {
+    public List<ProductDto> searchByKeyword(String keyword, Long lastSeenId, int size) {
 
-        return backToDayList(startOfDay, endOfDay, queryFactory ->
+        List<Product> products = backToDayList(startOfDay, endOfDay, queryFactory ->
                 queryFactory
                         .selectFrom(product)
+                        .leftJoin(product.prices, price).fetchJoin()
+                        .leftJoin(price.vendor, vendor).fetchJoin()
                         .where(
                                 product.name.containsIgnoreCase(keyword),
                                 lastSeenId != null ? product.id.gt(lastSeenId) : null,
                                 product.createdAt.between(startOfDay, endOfDay)
-                        ) // containsIgnoreCase 부분검색, like '%%'
+                        )
                         .orderBy(product.id.asc())
                         .limit(size)
                         .fetch()
         );
+        return products.stream()
+                .map(ProductConverter::toProductDTO)
+                .collect(Collectors.toList());
     }
 
     // 오늘의 최저가
     @Override
-    public List<Product> findLowestPriceProducts() {
-        return backToDayList(startOfDay, endOfDay, queryFactory ->
+    public List<ProductDto> findLowestPriceProducts() {
+
+        List<Product> products = backToDayList(startOfDay, endOfDay, queryFactory ->
                 queryFactory
                         .select(product)
                         .from(price)
                         .join(price.product, product)
+                        .join(price.vendor, vendor).fetchJoin()
                         .where(price.createdAt.between(startOfDay, endOfDay))
                         .orderBy(price.setPrice.asc())
                         .limit(10)
                         .fetch()
-        );
+                );
+        return products.stream()
+                .map(ProductConverter::toProductDTO)
+                .collect(Collectors.toList());
     }
 
     // 상품 상세 페이지
     @Override
-    public Product findProductWithLowestPriceByName(String productName) {
+    public ProductDto findProductWithLowestPriceByName(String productName) {
         List<Product> products = backToDayList(startOfDay, endOfDay, queryFactory ->
                 Collections.singletonList(queryFactory
                         .selectFrom(product)
-                        .join(product.prices, price)
+                                .leftJoin(product.prices, price).fetchJoin()
+                                .leftJoin(price.vendor, vendor).fetchJoin()
                         .where(product.name.eq(productName),
                                 price.createdAt.between(startOfDay, endOfDay)) // 상품명과 일치하는 상품만 조회
-                        .groupBy(product.id, price.vendor.id)
-                        .orderBy(product.name.asc(), price.setPrice.asc()) // 할인가 기준 최저가 정렬
+                                .orderBy(price.setPrice.asc())
                         .limit(1)
                         .fetchFirst()) // 정렬한 상품 중 첫번째 상품 반환
         );
 
-        return products.isEmpty() ? null : products.get(0);
+        return products.isEmpty() ? null : ProductConverter.toProductDTO(products.get(0));
     }
 
     // 카테고리 목록
     @Override
-    public List<Product> findByCategory(String category, Long lastSeenId, int size) {
+    public List<ProductDto> findByCategory(String category, Long lastSeenId, int size) {
         QProduct productSub = new QProduct("productSub");
         QPrice priceSub = new QPrice("priceSub");
 
-        return backToDayList(startOfDay, endOfDay, queryFactory ->
+        List<Product> products = backToDayList(startOfDay, endOfDay, queryFactory ->
                 queryFactory
                         .selectFrom(product)
-                        .join(product.prices, price)
+                        .leftJoin(product.prices, price).fetchJoin()
+                        .leftJoin(price.vendor, vendor).fetchJoin()
                         .where(
                                 product.category.eq(category),
                                 lastSeenId != null ? product.id.gt(lastSeenId) : null,
@@ -122,37 +137,43 @@ public class ProductRepositoryImpl implements ProductRepositoryCustom {
                         .orderBy(product.id.asc())
                         .limit(size)
                         .fetch()
-        );
+                );
+        return products.stream()
+                .map(ProductConverter::toProductDTO)
+                .collect(Collectors.toList());
     }
 
     // 판매처 리스트
     @Override
-    public List<Product> findProductWithVendors(String productName) {
-        return backToDayList(startOfDay, endOfDay, queryFactory ->
+    public List<ProductDto> findProductWithVendors(String productName) {
+
+        List<Product> products = backToDayList(startOfDay, endOfDay, queryFactory ->
                 queryFactory
                         .selectFrom(product)
-                        .leftJoin(product.prices, price)
-                        .fetchJoin()
-                        .where(product.name.eq(productName)
-                                , product.createdAt.between(startOfDay, endOfDay)
-                                , price.createdAt.between(startOfDay, endOfDay)
-                                ,vendor.createdAt.between(startOfDay, endOfDay))
+                        .leftJoin(product.prices, price).fetchJoin()
+                        .leftJoin(price.vendor, vendor).fetchJoin()
+                        .where(product.name.eq(productName),
+                                product.createdAt.between(startOfDay, endOfDay),
+                                price.createdAt.between(startOfDay, endOfDay),
+                                vendor.createdAt.between(startOfDay, endOfDay))
                         .fetch()
-        );
+                );
+        return products.stream()
+                .map(ProductConverter::toProductDTO)
+                .collect(Collectors.toList());
     }
 
     // 랜덤 추천
     @Override
-    public List<Product> findRandomProducts() {
-        LocalDateTime startOfDay = LocalDate.now().atStartOfDay();
-        LocalDateTime endOfDay = LocalDate.now().atTime(23, 59, 59, 999999999);
+    public List<ProductDto> findRandomProducts() {
 
-        return backToDayList(startOfDay, endOfDay, queryFactory ->
+        List<Product> products = backToDayList(startOfDay, endOfDay, queryFactory ->
                 queryFactory
                         .selectFrom(product)
-                        .join(product.prices, price)
+                        .leftJoin(product.prices, price).fetchJoin()
+                        .leftJoin(price.vendor, vendor).fetchJoin()
                         .where(
-                                product.createdAt.between(startOfDay, endOfDay), // 오늘의 생성된 상품만 필터링
+                                product.createdAt.between(startOfDay, endOfDay),
                                 price.setPrice.eq(
                                         JPAExpressions
                                                 .select(price.setPrice.min())
@@ -160,10 +181,12 @@ public class ProductRepositoryImpl implements ProductRepositoryCustom {
                                                 .where(price.product.eq(product))
                                 )
                         )
-                        .orderBy(Expressions.numberTemplate(Double.class, "function('RAND')").asc()) // 랜덤으로 섞기
-                        .limit(10) // 10개만 선택
+                        .orderBy(Expressions.numberTemplate(Double.class, "function('rand')").asc())
+                        .limit(10)
                         .fetch()
-        );
+                );
+        return products.stream()
+                .map(ProductConverter::toProductDTO)
+                .collect(Collectors.toList());
     }
-
 }
